09c44707108b1da614db3a9e8cf0f2b98962221c
hnmoura <hlsnm@discente.ifpe.edu.br>
2025-07-16 18:13:33 -0300

    Corrigido o erro no OnClick

--- src/components/MenuAdmin.jsx
+++ src/components/MenuAdmin.jsx
@@ -1,46 +1,45 @@
-import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import React from 'react';
 import { Menu, Icon } from 'semantic-ui-react';
 
-const MenuAdmin = ({ activeItem, onMenuClick }) => {
+const MenuAdmin = ({ activeItem = '', onMenuClick = () => {} }) => {
   return (
-    <>
-      <Menu vertical fixed='left' inverted style={{ height: '200vh', marginTop: '3.5em' }}>
-        <Menu.Item
-          name='dashboard'
-          active={activeItem === 'dashboard'}
-          onClick={() => onMenuClick('dashboard')}
-        >
-          <Icon name='user plus' />
-          Barbeiros
-        </Menu.Item>
-        <Menu.Item
-          name='barbeiros'
-          active={activeItem === 'barbeiros'}
-          onClick={() => onMenuClick('formBarbeiro')}
-        >
-          <Icon name='users' />
-          Clientes
-        </Menu.Item>
-        <Menu.Item
-          name='agendamentos'
-          active={activeItem === 'agendamentos'}
-          onClick={() => onMenuClick('agendamentos')}
-        >
-          <Icon
-            name='clipboard list' />
-          Serviços
-        </Menu.Item>
-        <Menu.Item
-          name='servicos'
-          active={activeItem === 'servicos'}
-          onClick={() => onMenuClick('CadastroServicos')}
-        >
-          <Icon name='calendar alternate' />
-          Agendamentos
-        </Menu.Item>
-      </Menu>
-    </>
+    <Menu vertical fixed='left' inverted style={{ height: '200vh', marginTop: '3.5em' }}>
+      <Menu.Item
+        name='dashboard'
+        active={activeItem === 'dashboard'}
+        onClick={() => onMenuClick('dashboard')}
+      >
+        <Icon name='dashboard' />
+        Dashboard
+      </Menu.Item>
+
+      <Menu.Item
+        name='barbeiros'
+        active={activeItem === 'barbeiros'}
+        onClick={() => onMenuClick('barbeiros')}
+      >
+        <Icon name='users' />
+        Clientes
+      </Menu.Item>
+
+      <Menu.Item
+        name='servicos'
+        active={activeItem === 'servicos'}
+        onClick={() => onMenuClick('servicos')}
+      >
+        <Icon name='clipboard list' />
+        Serviços
+      </Menu.Item>
+
+      <Menu.Item
+        name='agendamentos'
+        active={activeItem === 'agendamentos'}
+        onClick={() => onMenuClick('agendamentos')}
+      >
+        <Icon name='calendar alternate' />
+        Agendamentos
+      </Menu.Item>
+    </Menu>
   );
 };
 
